94a40d31d024f181a765e9304fd8f7e77770d2fa
Stas Goshovsky <stasgosha@yandex.ru>
2021-04-15 12:35:48 +0300

    - basic layout

---
+++ js/scripts.js
@@ -0,0 +1,366 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+	const isRTL = $('html').attr('dir') == 'rtl';
+	const isMobile = $(window).width() < 992;
+
+	if (isRTL) {
+		$('.wpcf7').attr('dir','rtl');
+	} else{
+		$('.wpcf7').attr('dir','ltr');
+	}
+
+	function is_touch_device() {
+		try {
+			document.createEvent("TouchEvent");
+			return true;
+		} catch (e) {
+			return false;
+		}
+	}
+
+	if(is_touch_device()){
+		$('body').addClass('touch');
+	} else{
+		$('body').addClass('no-touch');
+	}
+
+	// Wow
+	// new WOW().init();
+	function animationInit(){
+		let i = 0;
+		let intv = setInterval(function(){
+			$('.product-stats-block .item').eq(i).addClass('animated');
+			i++;
+
+			if (i==4) {
+				clearInterval(intv);
+			}
+		}, 1000);
+	}
+
+	animationInit();
+
+	// Sticky btn
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > $('#contacts').offset().top - 100) {
+			$('.show-form-btn').addClass('hide');
+		} else{
+			$('.show-form-btn').removeClass('hide');
+		}
+	});
+
+	function getMaxOfArray(numArray) {
+		return Math.max.apply(null, numArray);
+	}
+
+	// Fields
+	$('.form-field input').on('change keyup', function(e){
+		if ($(this).val() !== '') {
+			$(this).addClass('not-empty');
+		} else{
+			$(this).removeClass('not-empty');
+		}
+	});
+
+	// Sliders
+	function equalSlideHeight(slider){
+		$(slider).on('setPosition', function () {
+			$(this).find('.slick-slide').height('auto');
+			var slickTrack = $(this).find('.slick-track');
+			var slickTrackHeight = $(slickTrack).height();
+			$(this).find('.slick-slide').css('height', slickTrackHeight + 'px');
+		});
+	}
+
+	let arrowsButtons = {
+		prevArrow: '<button type="button" class="slick-prev" aria-label="Previous"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60.1 62"><defs><filter id="a" x="20" y="0" width="40.1" height="62" filterUnits="userSpaceOnUse"><feOffset/><feGaussianBlur stdDeviation="3" result="blur"/><feFlood flood-opacity=".2"/><feComposite operator="in" in2="blur"/><feComposite in="SourceGraphic"/></filter><filter id="b" x="0" y="0" width="40.1" height="62" filterUnits="userSpaceOnUse"><feOffset/><feGaussianBlur stdDeviation="3" result="blur-2"/><feFlood flood-opacity=".2"/><feComposite operator="in" in2="blur-2"/><feComposite in="SourceGraphic"/></filter></defs><g filter="url(#a)"><path d="M30.1 52l19-21-19-21" fill="none" stroke="#00a0be" stroke-miterlimit="10" stroke-width="3"/></g><g filter="url(#b)"><path d="M10.1 52l19-21-19-21" fill="none" stroke="#00a0be" stroke-miterlimit="10" stroke-width="3"/></g></svg></button>',
+		nextArrow: '<button type="button" class="slick-next" aria-label="Next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60.1 62"><defs><filter id="a" x="20" y="0" width="40.1" height="62" filterUnits="userSpaceOnUse"><feOffset/><feGaussianBlur stdDeviation="3" result="blur"/><feFlood flood-opacity=".2"/><feComposite operator="in" in2="blur"/><feComposite in="SourceGraphic"/></filter><filter id="b" x="0" y="0" width="40.1" height="62" filterUnits="userSpaceOnUse"><feOffset/><feGaussianBlur stdDeviation="3" result="blur-2"/><feFlood flood-opacity=".2"/><feComposite operator="in" in2="blur-2"/><feComposite in="SourceGraphic"/></filter></defs><g filter="url(#a)"><path d="M30.1 52l19-21-19-21" fill="none" stroke="#00a0be" stroke-miterlimit="10" stroke-width="3"/></g><g filter="url(#b)"><path d="M10.1 52l19-21-19-21" fill="none" stroke="#00a0be" stroke-miterlimit="10" stroke-width="3"/></g></svg></button>'
+	}
+
+	function dotsOnArc(dots, direction = 1){
+		let buttons = $(dots).find('button');
+		let dotsCount = buttons.length;
+		let middleEl;
+		let step = 15 / $(window).width() * 100 * direction;
+
+
+		if (dotsCount % 2 == 1) {
+			middleEl = (dotsCount - 1) / 2;
+		} else{
+			middleEl = dotsCount / 2;
+			buttons.splice(middleEl-1, 1);
+
+			middleEl = (buttons.length - 1) / 2;
+		}
+
+		for(let i = 1; i <= middleEl; i++){
+			buttons.eq(middleEl + i).css({
+				'transform': `translateY(${step * i}px)`
+			});
+
+			buttons.eq(middleEl - i).css({
+				'transform': `translateY(${step * i}px)`
+			});
+		}
+	}
+
+	$('.testimonials-slider-wrapper').each(function(i, el){
+		$(el).find('.testimonials-slider').on('init reInit', function(){
+			dotsOnArc($(el).find('.slick-dots'), -1);
+		});
+
+		$(el).find('.testimonials-slider').slick({
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			arrows: true,
+			...arrowsButtons,
+			appendArrows: $(el).find('.slider-nav'),
+			dots: true,
+			appendDots: $(el).find('.slider-nav'),
+			infinite: true,
+			speed: 800,
+			// autoplay: true,
+			// autoplaySpeed: 5000,
+			rtl: true,
+			centerMode: true,
+			centerPadding: 0,
+			swipeToSlide: true,
+			focusOnSelect: true,
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 3,
+						arrows: false,
+						dots: false,
+						centerMode: false,
+						swipeToSlide: false
+					}
+				}
+			]
+		});
+	});
+
+	$('.videos-slider-wrapper').each(function(i, el){
+		$(el).find('.videos-slider').on('init reInit', function(){
+			dotsOnArc($(el).find('.slick-dots'), 1);
+		});
+
+		$(el).find('.videos-slider').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			arrows: true,
+			...arrowsButtons,
+			appendArrows: $(el).find('.slider-nav'),
+			dots: true,
+			appendDots: $(el).find('.slider-nav'),
+			infinite: true,
+			speed: 800,
+			// autoplay: true,
+			// autoplaySpeed: 5000,
+			rtl: isRTL,
+			centerMode: true,
+			centerPadding: 0,
+			swipeToSlide: true,
+			focusOnSelect: true,
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 3,
+						arrows: false,
+						dots: false,
+						centerMode: false,
+						swipeToSlide: false
+					}
+				}
+			]
+		});
+	});
+
+	if (isMobile) {
+		$('.first-screen-slider').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			dots: false,
+			infinite: true,
+			speed: 800,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			rtl: true
+		});
+	}
+
+	// Scroll to anchor
+	$(document).on('click', 'a[href^="#"]', function (event) {
+		event.preventDefault();
+
+		if ($.attr(this, 'href') === '#') {
+			return false;
+		}
+
+		let offset = 0;
+
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top - offset
+		}, 500);
+	});
+
+	// Menu opener
+	$('.menu-opener').click(function(e){
+		e.preventDefault();
+
+		$('.menu-opener').toggleClass('active');
+		$('.mobile-top-nav').toggleClass('opened');
+		$('.header').toggleClass('nav-opened');
+	});
+
+	// Sticky Header
+	function stickyHeader(){
+		let header = document.querySelector('.header');
+
+		if (!!header) {
+			window.scrollY > 100
+				? header.classList.add('sticky')
+				: header.classList.remove('sticky');
+		};
+	}
+
+
+	window.addEventListener('scroll', stickyHeader);
+	setTimeout(stickyHeader, 100);
+
+	// Modals
+	$('.modal').css('display','block');
+
+	$('.modal-dialog').click(e => e.stopPropagation());
+	$('.modal').click(function(e){
+		hideModal( $(this) );
+		$('.video-modal .modal-video').html('<div id="modal-video-iframe"></div>');
+	});
+
+	$('.modal-close, .js-modal-close').click(function(e){
+		e.preventDefault();
+
+		hideModal( $(this).closest('.modal') );
+		$('.video-modal .modal-video').html('<div id="modal-video-iframe"></div>');
+	});
+
+	$('[data-modal]').click(function(e){
+		e.preventDefault();
+		e.stopPropagation();
+
+		hideModal('.modal');
+
+		if ($(this).data('modal-tab') != undefined) {
+			goToTab($(this).data('modal-tab'));
+		}
+
+		showModal( $(this).data('modal') );
+	});
+
+	// Video
+	$('.video-block:not([data-video-modal])').on('click', function () {
+		$(this).addClass('playing');
+		$(this).find('.block-overlay').fadeOut(300);
+
+		let videoId = $(this).find('.play-btn').data('video-id');
+
+		if (!videoId) {
+			videoId = $(this).data('video-id');
+		}
+
+		if (videoId == undefined) {
+			$(this).find('video')[0].play();
+		} else{
+			let videoType = $(this).data('video-type') ? $(this).data('video-type').toLowerCase() : 'youtube';
+
+			if (videoType == 'youtube') {
+				$(this).find('.block-video-container').append('<div class="video-iframe" id="'+videoId+'"></div>');
+				createVideo(videoId, videoId);
+			} else if(videoType == 'vimeo'){
+				$(this).find('.block-video-container').append('<div class="video-iframe" id="'+videoId+'"><iframe allow="autoplay" class="video-iframe" src="https://player.vimeo.com/video/'+videoId+'?playsinline=1&autoplay=1&transparent=0&app_id=122963"></div>');
+			}
+		}
+	});
+
+	$('[data-video-modal]').click(function(e){
+		e.preventDefault();
+		e.stopPropagation();
+
+		let videoId = $(this).data('video-modal');
+		let videoType = $(this).data('video-type');
+
+		if (videoType == 'youtube') {
+			$('#modal-video-iframe').removeClass('vimeo youtube').addClass('youtube').append('<div class="video-iframe" id="'+videoId+'"></div>');
+			createVideo(videoId, videoId);
+		} else if(videoType == 'vimeo'){
+			$('#modal-video-iframe').removeClass('vimeo youtube').addClass('vimeo').html('<iframe class="video-iframe" allow="autoplay" src="https://player.vimeo.com/video/'+videoId+'?playsinline=1&autoplay=1&transparent=1&app_id=122963">');
+		}
+
+		hideModal('.modal');
+
+		showModal( "#video-modal" );
+	});
+
+	var player;
+	function createVideo(videoBlockId, videoId) {
+		player = new YT.Player(videoBlockId, {
+			videoId: videoId,
+			playerVars: {
+				'autohide': 1,
+				'showinfo' : 0,
+				'rel': 0,
+				'loop': 1,
+				'playsinline': 1,
+				'fs': 0,
+				'allowsInlineMediaPlayback': true
+			},
+			events: {
+				'onReady': function (e) {
+					// e.target.mute();
+					// if ($(window).width() > 991) {
+						setTimeout(function(){
+							e.target.playVideo();
+						}, 200);
+					// }
+				}
+			}
+		});
+	}
+});
+
+
+function getScrollWidth(){
+	// create element with scroll
+	let div = document.createElement('div');
+
+	div.style.overflowY = 'scroll';
+	div.style.width = '50px';
+	div.style.height = '50px';
+
+	document.body.append(div);
+	let scrollWidth = div.offsetWidth - div.clientWidth;
+
+	div.remove();
+
+	return scrollWidth;
+}
+
+let bodyScrolled = 0;
+function showModal(modal){
+	$(modal).addClass('visible');
+	bodyScrolled = $(window).scrollTop();
+	$('body').addClass('modal-visible')
+			 .scrollTop(bodyScrolled)
+			 .css('padding-right', getScrollWidth());
+}
+
+function hideModal(modal){
+	$(modal).removeClass('visible');
+	bodyScrolled = $(window).scrollTop();
+	$('body').removeClass('modal-visible')
+			 .scrollTop(bodyScrolled)
+			 .css('padding-right', 0);
+}
